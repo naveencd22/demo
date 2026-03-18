1236a620a070d4afa2bc440582e18177c6867f8b
Naveen Chowdary <naveencd22@gmail.com>
2026-03-18 10:52:08 +0530

    removed deprecated methods

--- src/main/java/com/example/demo/configs/SecurityConfiguration.java
+++ src/main/java/com/example/demo/configs/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -31,18 +32,17 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf()
-                .disable()
-                .authorizeHttpRequests()
-                // Allow unauthenticated access to the /auth/** and /actuator/** endpoints, while requiring authentication for all other requests
-                .requestMatchers("/auth/**", "/actuator/**","/api-docs/**", "/swagger-ui/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                    // Allow unauthenticated access to the /auth/** and /actuator/** endpoints, while requiring authentication for all other requests
+                    .requestMatchers("/auth/**", "/actuator/**","/api-docs/**", "/swagger-ui/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated()
+                )
+                .sessionManagement(sess -> sess
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 // Configure the authentication provider and add the JWT authentication filter to the security filter chain
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
